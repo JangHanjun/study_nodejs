bb8b0229239dcfb83976809ebbaaf801d5994fc3
husk <68003176+JangHanjun@users.noreply.github.com>
2020-11-09 17:55:32 +0900

    2장 완료...

---
+++ views/middlewares.js
@@ -0,0 +1,11 @@
+import routes from "./routes";
+// routes에 있는 것들을 전역적으로 사용할 수 있게 해줌
+export const localsMiddleware = (req, res, next) => {
+     res.locals.siteName = 'Wetube';
+     res.locals.routes = routes;
+     res.locals.user={
+          isAuthenticated: true,
+          id: 1
+     };
+     next();
+};
